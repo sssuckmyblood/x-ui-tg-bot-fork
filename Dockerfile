ad7038317c7e419a1f53ed19b372e0a4278134ba
sssuckmyblood <testkingref@gmail.com>
2024-10-16 00:52:04 +0700

    fix self build docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM golang:1.23-alpine AS builder
 WORKDIR /app
-ARG TARGETARCH 
-RUN apk --no-cache --update add build-base gcc wget unzip
+ARG TARGETARCH=amd64
+ENV TARGETARCH=$TARGETARCH
+RUN apk --no-cache --update add build-base gcc wget unzip 
 COPY . .
 ENV CGO_ENABLED=1
 ENV CGO_CFLAGS="-D_LARGEFILE64_SOURCE"
 RUN go build -o build/x-ui main.go
-RUN ./DockerInitFiles.sh "$TARGETARCH"
+RUN echo "TARGETARCH: $TARGETARCH" && ls -la && ./aye.sh "$TARGETARCH"
+
 
 FROM alpine
 LABEL org.opencontainers.image.authors="alireza7@gmail.com"
--- web/service/tgbot.go
+++ web/service/tgbot.go
@@ -217,6 +217,8 @@ func (t *Tgbot) answerCallback(callbackQuery *tgbotapi.CallbackQuery) {
 		t.getClientUsage(callbackQuery.From.ID, callbackQuery.From.UserName)
 	case "client_commands":
 		t.SendMsgToTgbot(callbackQuery.From.ID, t.I18nBot("tgbot.commands.helpClientCommands"))
+	case "client_profile":
+		t.SendMsgToTgbot(callbackQuery.From.ID, t.I18nBot("tgbot.commands.status"))
 	case "onlines":
 		t.onlineClients(callbackQuery.From.ID)
 	case "commands":
@@ -254,6 +256,7 @@ func (t *Tgbot) SendAnswer(chatId int64, msg string, isAdmin bool) {
 	)
 	numericKeyboardClient := tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(t.I18nBot("tgbot.buttons.clientProfile"), "client_profile"),
 			tgbotapi.NewInlineKeyboardButtonData(t.I18nBot("tgbot.buttons.clientUsage"), "client_traffic"),
 			tgbotapi.NewInlineKeyboardButtonData(t.I18nBot("tgbot.buttons.commands"), "client_commands"),
 		),
